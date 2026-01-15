1dc4e616ba63f739dd2d37bd8c2987151d68a3b3
Huzaifa Tahir <145917168+huzaifa-tahir-40@users.noreply.github.com>
2026-01-15 20:39:23 +0500

    Add files via upload

---
+++ main/ik_3dof.ino
@@ -0,0 +1,70 @@
+#include <math.h>
+void ik_3dof(double Px, double Py, double Pz, double a1, double a2, double a3, double q[3][2])
+{
+/*
+- This function is used to perform inverse kinematics calculations
+- Px, Py, Pz are XYZ coordinate position where the end-effector should move to.
+- a1, a2, a3 are link lengths.
+- q[3][2] is an array of possible joint angles for the desired XYZ coordinate position.
+- There are two possible joint angle answers for each coordinate position in the workspace.
+*/
+  //Calculate the Angle for Joint 1 (Thetha 1) 
+  double q1 = atan2(Py, Px);
+  //Projection of Universal Frame on Target Frame
+  double r  = sqrt(pow(Px,2) + pow(Py,2));
+
+  //Intermediate parameters
+  double k1 = Px / cos(q1);
+  double k2 = Py / sin(q1);
+  double k3 = Pz - a1;
+  double k;
+
+  if(cos(q1) < 0.01)
+    k = k2;
+  else if(sin(q1) < 0.01)
+    k = k1;
+
+  double cos_q3 = ((k1*k1) + (k3*k3) - (a2*a2) - (a3*a3)) / (2 * a2 * a3);
+  double sin_q3 = sqrt(1 - (cos_q3*cos_q3));
+
+  //Joint Angles for Elbow Down and Elbow Up Configuration.
+  double q3_d = atan2( sin_q3 , cos_q3 );  //Theta 3 for Elbow Down
+  double q3_u = atan2(-sin_q3, cos_q3 );  // Theta 3 for Elbow Up
+
+  double q2_d = atan2(k3, r) - atan2(a3*sin(q3_d), a2 + a3*cos(q3_d)); //Theta 2 for Elbow Down
+  double q2_u = atan2(k3, r) - atan2(a3*sin(q3_u), a2 + a3*cos(q3_u)); //Thata 2 for Elbow Up
+
+  // For Displaying Output in Serial Monitor
+  Serial.println("====================== IK Calculations ==========================");
+  Serial.println("====================== Parameters      ==========================");
+  Serial.print("k1 = "); Serial.println(k1);
+  Serial.print("k2 = "); Serial.println(k2);
+  Serial.print("k3 = "); Serial.println(k3);
+  Serial.print("r = "); Serial.println(r);
+  Serial.print("cos_q3 = "); Serial.println(cos_q3);
+  Serial.print("sin_q3 = "); Serial.println(sin_q3);
+
+  // For Displaying Elbow Up Output in Serial Monitor
+  Serial.println("====================== UP              ==========================");
+  Serial.print("q1 = "); Serial.println(round(rad2deg(q1)));
+  Serial.print("q2_up = "); Serial.println(round(rad2deg(q2_u)));
+  Serial.print("q3_up = "); Serial.println(round(rad2deg(q3_u)));
+  
+  // For Displaying Elow Down Output in Serial Monitor
+  Serial.println("====================== DOWN            ==========================");
+  Serial.print("q1 = "); Serial.println(round(rad2deg(q1)));
+  Serial.print("q2_d = "); Serial.println(round(rad2deg(q2_d)));
+  Serial.print("q3_d = "); Serial.println(round(rad2deg(q3_d)));
+  Serial.println("====================== END             ==========================");
+
+  //Store Result in Output Matrix
+  //Elbow Up Configuration 
+  q[0][0] = q1;
+  q[1][0] = q2_u;
+  q[2][0] = q3_u;
+
+  //Elbow Down Configuration
+  q[0][1] = q1;
+  q[1][1] = q2_d;
+  q[2][1] = q3_d;
+}
---
+++ main/rad2deg.ino
@@ -0,0 +1,7 @@
+const float pi = 3.14268;
+
+// This converts Angles from Radians to Degrees
+int rad2deg(float rad)
+{
+  return int(rad * 180 / pi);
+}
