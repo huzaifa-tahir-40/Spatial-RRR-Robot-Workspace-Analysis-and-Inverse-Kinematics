6b53a0740a93310597c01cb9f65e0d2f4f2e14f8
Huzaifa Tahir <145917168+huzaifa-tahir-40@users.noreply.github.com>
2026-01-15 20:54:36 +0500

    Enhance ik_verify function with descriptive comments
    Added comments to explain the purpose of the ik_verify function.

--- MATLAB/ik_verify.m
+++ MATLAB/ik_verify.m
@@ -1,4 +1,6 @@
 function p_check = ik_verify(q, a)
+% It performs forward kinematics to get the position vector for given joint angles.
+% Purpose: to validate whether the joint angles from inverse kinematics are correct or not.
     l1 = a(1); l2 = a(2); l3 = a(3);
     q1 = q(1); q2 = q(2); q3 = q(3);
 
@@ -7,5 +9,6 @@ function p_check = ik_verify(q, a)
     z = l1 + l3*sin(q2 + q3) + l2*sin(q2);
     
     p_check = [x, y, z];
+
 
 end
