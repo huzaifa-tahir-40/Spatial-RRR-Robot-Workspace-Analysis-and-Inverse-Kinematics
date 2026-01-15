3023534dac0eb3c62c57890ed33935e69f644b67
Huzaifa Tahir <145917168+huzaifa-tahir-40@users.noreply.github.com>
2026-01-15 20:49:16 +0500

    Restore ik_3dof function with detailed comments
    Restored the ik_3dof function and its comments for inverse kinematics calculations.

--- main/ik_3dof.ino
+++ main/ik_3dof.ino
@@ -17,8 +17,9 @@ void ik_3dof(double Px, double Py, double Pz, double a1, double a2, double a3, d
   double k1 = Px / cos(q1);
   double k2 = Py / sin(q1);
   double k3 = Pz - a1;
-  double k;
+  double k;              // it will be used in place of k1 or k2. (k1 = k2)
 
+  // This if-else condition avoids division by a tiny number. Because otherwise k1 and k2 will not be equal
   if(cos(q1) < 0.01)
     k = k2;
   else if(sin(q1) < 0.01)
@@ -28,11 +29,11 @@ void ik_3dof(double Px, double Py, double Pz, double a1, double a2, double a3, d
   double sin_q3 = sqrt(1 - (cos_q3*cos_q3));
 
   //Joint Angles for Elbow Down and Elbow Up Configuration.
-  double q3_d = atan2( sin_q3 , cos_q3 );  //Theta 3 for Elbow Down
-  double q3_u = atan2(-sin_q3, cos_q3 );  // Theta 3 for Elbow Up
+  double q3_d = atan2( sin_q3 , cos_q3 );  // Theta 3 for Elbow Down
+  double q3_u = atan2(-sin_q3, cos_q3 );   // Theta 3 for Elbow Up
 
-  double q2_d = atan2(k3, r) - atan2(a3*sin(q3_d), a2 + a3*cos(q3_d)); //Theta 2 for Elbow Down
-  double q2_u = atan2(k3, r) - atan2(a3*sin(q3_u), a2 + a3*cos(q3_u)); //Thata 2 for Elbow Up
+  double q2_d = atan2(k3, r) - atan2(a3*sin(q3_d), a2 + a3*cos(q3_d)); // Theta 2 for Elbow Down
+  double q2_u = atan2(k3, r) - atan2(a3*sin(q3_u), a2 + a3*cos(q3_u)); // Thata 2 for Elbow Up
 
   // For Displaying Output in Serial Monitor
   Serial.println("====================== IK Calculations ==========================");
@@ -57,14 +58,15 @@ void ik_3dof(double Px, double Py, double Pz, double a1, double a2, double a3, d
   Serial.print("q3_d = "); Serial.println(round(rad2deg(q3_d)));
   Serial.println("====================== END             ==========================");
 
-  //Store Result in Output Matrix
-  //Elbow Up Configuration 
+  // Store Result in Output Matrix
+  // Elbow Up Configuration 
   q[0][0] = q1;
   q[1][0] = q2_u;
   q[2][0] = q3_u;
 
-  //Elbow Down Configuration
+  // Elbow Down Configuration
   q[0][1] = q1;
   q[1][1] = q2_d;
   q[2][1] = q3_d;
 }
+
